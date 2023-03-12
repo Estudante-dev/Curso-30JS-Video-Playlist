64f26eff77a7720db6b9cbf87a8e8555442a0951
ecossistema.dev <107730006+ecossistemadev@users.noreply.github.com>
2023-03-12 14:43:43 -0300

    update iframe

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let videoPrincipal = document.querySelector("video");
+let videoPrincipal = document.querySelector("iframe");
 let videosPlaylist = document.querySelector(".videosPlaylist");
 let videoTitulo = document.querySelector(".titulo");
 
@@ -6,28 +6,28 @@ let videos = [
     {
       'id': 'a1',
       'titulo': 'Dois gatinhos e vários pintinhos',
-      'url': 'video-01.mp4'
+      'url': 'https://www.youtube.com/embed/O58N8Mmv0QU'
     },
     {
       'id': 'a2',
       'titulo': 'Filhotinhos Fofos',
-      'url': 'video-02.mp4'
+      'url': 'https://www.youtube.com/embed/jANE8lpoj2c'
     },
     {
       'id': 'a3',
       'titulo': 'Um gatinho e um patinho',
-      'url': 'video-03.mp4'
+      'url': 'https://www.youtube.com/embed/4ai8-m9Dp_8'
     },
   
     {
       'id': 'a4',
       'titulo': 'Muitos patinhos e um gatinho',
-      'url': 'video-04.mp4'
+      'url': 'https://www.youtube.com/embed/Vyfx9FwmHMY'
     },
     {
       'id': 'a5',
       'titulo': 'Gatinhos divertidos',
-      'url': 'video-05.mp4'
+      'url': 'https://www.youtube.com/embed/LMD6MqwErzc'
     }
   
   ];
@@ -43,7 +43,7 @@ let videos = [
 
   function videoSelecionado(id) {
     let videoSelecionadoId = videos.find(video => video.id === id); // Procura o objeto de vídeo correspondente ao ID selecionado
-    videoPrincipal.src = `videos/${videoSelecionadoId.url}`; // Define o src do elemento de vídeo principal com o nome do arquivo de vídeo correspondente
+    videoPrincipal.src = `${videoSelecionadoId.url}`; // Define o src do elemento de vídeo principal com o nome do arquivo de vídeo correspondente
     videoTitulo.innerHTML = videoSelecionadoId.titulo; // Define o título do vídeo principal com o título do vídeo selecionado
   
   }
